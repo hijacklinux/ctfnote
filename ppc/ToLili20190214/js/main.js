39ef479ec238b978f9e83baaed852018d5f82ad9
hijacklinux <handjane@163.com>
2019-02-07 17:32:53 +0800

    Update main.js

--- ppc/ToLili20190214/js/main.js
+++ ppc/ToLili20190214/js/main.js
@@ -224,7 +224,7 @@ render_gameover();
 			context.save();
 			context.fillStyle = "#000000";
 			var img = new Image();
-			img.src = "https://raw.githubusercontent.com/hijacklinux/ctfnote/master/ppc/ToLili20190214/qrcode.png"
+			img.src = "https://raw.githubusercontent.com/hijacklinux/ctfnote/master/ppc/ToLili20190214/qrcode.png";
 			img.onload=function(){context.drawImage(img,10,10};
 			
 			context.restore();
